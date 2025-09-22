cd4c3688d8cdf8a227a34884278a2c0f874bde72
sittafatim4 <sitta.fatimaa@gmail.com>
2025-09-22 23:04:56 +0700

    Initial commit: setup BMI Calculator project

---
+++ bin/main.dart
@@ -0,0 +1,43 @@
+// bin/main.dart
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+
+  // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+}
+
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
+  double tinggiM = tinggiCm / 100;
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
+  riwayat.add({
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori,
+  });
+}
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  print("=== Riwayat Perhitungan BMI ===");
+  for (var data in riwayat) {
+    print("Tinggi: ${data['tinggi']} cm | Berat: ${data['berat']} kg | "
+          "BMI: ${data['bmi'].toStringAsFixed(2)} | Kategori: ${data['kategori']}");
+  }
+}
