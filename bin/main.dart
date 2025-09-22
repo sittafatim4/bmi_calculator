ec0814a362e86a3a46dfd96f67e1cba676a4daaa
sittafatim4 <sitta.fatimaa@gmail.com>
2025-09-22 23:11:20 +0700

    Tambah fitur perhitungan BMI sesuai rumus WHO

--- bin/main.dart
+++ bin/main.dart
@@ -13,18 +13,18 @@ void main() {
 
 void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   double tinggiM = tinggiCm / 100;
-  double bmi = beratKg / (tinggiM * tinggiM);
+double bmi = beratKg / (tinggiM * tinggiM);
 
-  String kategori;
-  if (bmi < 18.5) {
-    kategori = "Kurus";
-  } else if (bmi < 25) {
-    kategori = "Normal";
-  } else if (bmi < 30) {
-    kategori = "Gemuk";
-  } else {
-    kategori = "Obesitas";
-  }
+String kategori;
+if (bmi < 18.5) {
+  kategori = "Kurus";
+} else if (bmi < 25) {
+  kategori = "Normal";
+} else if (bmi < 30) {
+  kategori = "Gemuk";
+} else {
+  kategori = "Obesitas";
+}
 
   riwayat.add({
     'tinggi': tinggiCm,
